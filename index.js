6768ac3617b0eb22fb01bb14920dff7dea183c09
codebonding <codebonding@gmail.com>
2025-10-06 12:44:33 +0530

    fc

--- index.js
+++ index.js
@@ -23,4 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 sequelize.sync({ alter: true }).then(() => console.log('DB synced'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
